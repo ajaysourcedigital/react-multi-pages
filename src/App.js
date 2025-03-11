e8a0a1018b6c5d53d7a3d2ada45a5cf9bbbf32d6
Ajay Kumar <56859399+ajaysourcedigital@users.noreply.github.com>
2025-03-11 10:00:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { browserTracker } from '@pandastack/event-tracker';
 
 class App extends Component {
   componentDidMount() {
-    if (window.Analytics) return;
-    const script = document.createElement('script');
-    script.src = 'https://cdn.pandastack.in/analytics.js';
-    script.async = true;
-    script.onload = () => {
-      if (window.Analytics) {
-        window.Analytics.init();  // Added window. prefix here
-      }
-    };
-    document.head.appendChild(script);
+    // Initialize the analytics tracker when component mounts
+    browserTracker.init({ project_id: "49ac8def-eda3-4286-bb8f-a6abaaa70a76" });
   }
 
   render() {
@@ -21,7 +14,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React- before rollback</h1>
+          <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
